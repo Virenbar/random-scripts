d9b25befb24e87ba824e1952d6a63334bf721955
Virenbar <Virenbar@users.noreply.github.com>
2024-04-17 15:33:01 +0500

    Update EnergyMonitor

--- Lua-ComputerCraft/Drama/ct.lua
+++ Lua-ComputerCraft/Drama/ct.lua
@@ -1,45 +1,45 @@
 module = {}
 
 function module.print(mon, sText)
-  local w,h = mon.getSize()
-  local x,y = mon.getCursorPos()
+  local w, h = mon.getSize()
+  local x, y = mon.getCursorPos()
   local str = ""
   local function newLine()
     x, y = mon.getCursorPos()
-    mon.setCursorPos((math.floor(w/2) - (math.floor(#str/2)))+1, y)
+    mon.setCursorPos((math.floor(w / 2) - (math.floor(#str / 2))) + 1, y)
     mon.write(str)
     str = ""
     mon.setCursorPos(1, y + 1)
     x, y = mon.getCursorPos()
   end
-  if #sText<w then   
+  if #sText < w then
     str = sText
     newLine()
   else
     -- Print the line with proper word wrapping
     while string.len(sText) > 0 do
-      local whitespace = string.match(sText, "%s*")--"^[ t]+"
+      local whitespace = string.match(sText, "%s*") --"^[ t]+"
       if whitespace then
         -- Print whitespace
-        str = str..whitespace
+        str = str .. whitespace
         --mon.write(whitespace)
-        x,y = mon.getCursorPos()
+        x, y = mon.getCursorPos()
         sText = string.sub(sText, string.len(whitespace) + 1)
       end
-        
-      local text = string.match(sText, "%S*")--"^[^ tn]+" 
+
+      local text = string.match(sText, "%S*") --"^[^ tn]+"
       if text then
         sText = string.sub(sText, string.len(text) + 1)
         if string.len(text) > w then
-          -- Print a multiline word                
-          while string.len( text ) > 0 do
+          -- Print a multiline word
+          while string.len(text) > 0 do
             if #str > w then
               newLine()
             end
             --mon.write(text)
-            str = str..text
-            text = string.sub(text, (w-#str) + 2)
-            x,y = mon.getCursorPos()
+            str = str .. text
+            text = string.sub(text, (w - #str) + 2)
+            x, y = mon.getCursorPos()
           end
         else
           -- Print a word normally
@@ -47,8 +47,8 @@ function module.print(mon, sText)
             newLine()
           end
           --mon.write(text)
-          str = str..text
-          x,y = mon.getCursorPos()
+          str = str .. text
+          x, y = mon.getCursorPos()
         end
       end
       --print(#str)
--- Lua-ComputerCraft/EnergyMonitor/EnergyMonitor.lua
+++ Lua-ComputerCraft/EnergyMonitor/EnergyMonitor.lua
@@ -1,38 +1,51 @@
 local R = 1
 local w1 = 6
 local w2 = 10
---elite_energy_cube
+--
+local toFE = mekanismEnergyHelper.joulesToFE
 local output = 'left'
 local cube = peripheral.find("inductionPort")
-local monitors = {peripheral.find("monitor")}
-local oldEnergy = cube.getEnergy()
-local maxEnergy = cube.getMaxEnergy()
+local monitors = { peripheral.find("monitor") }
+--
 local mon = {}
+local percent = 0
+local energy = 0
+local change = 0
+local maxEnergy = toFE(cube.getMaxEnergy())
 --
 function reset()
   mon.clear()
-  mon.setCursorPos(1,1)
+  mon.setCursorPos(1, 1)
   mon.setTextColor(colors.white)
 end
+
 function newline()
-  x, y = mon.getCursorPos()
-  mon.setCursorPos(1,y+1)
+  local x, y = mon.getCursorPos()
+  mon.setCursorPos(1, y + 1)
   mon.setTextColor(colors.white)
 end
+
 function rpad1(s)
   s = tostring(s)
-  local pad=w1-#s
-  return string.rep(" ",pad) .. s
+  local pad = w1 - #s
+  return string.rep(" ", pad) .. s
 end
+
 function rpad2(s)
   s = tostring(s)
-  local pad=w2-#s
-  return string.rep(" ",pad) .. s
+  local pad = w2 - #s
+  return string.rep(" ", pad) .. s
 end
+
 function round(num, idp)
-  local mult = 10^(idp or 0)
+  local mult = 10 ^ (idp or 0)
   return math.floor(num * mult + 0.5) / mult
 end
+
+function ternary(condition, T, F)
+  if condition then return T else return F end
+end
+
 function formatEnergy(n)
   --local placeValue = ("%%.%df"):format(places or 0)
   if not n then
@@ -51,69 +64,76 @@ function formatEnergy(n)
 end
 
 --Modifyed print
-function printnocolor(name,value,unit)
-  mon.write(rpad1(name)..": "..rpad2(tostring(round(value,3))).." "..unit)
-  newline()
-end
-function printcolor(name,value,maxvalue,unit)
-  mon.write(rpad1(name)..": ")
-  local percent = math.floor(value/math.abs(maxvalue)*100)
-  if maxvalue>0 then
-    if percent > 66 then mon.setTextColor(colors.green)
-    elseif percent < 34 then mon.setTextColor(colors.red)
-    else mon.setTextColor(colors.yellow)
-    end
-  else
-    if percent > 66 then mon.setTextColor(colors.red)
-    elseif percent < 34 then mon.setTextColor(colors.green)
-    else mon.setTextColor(colors.yellow)
-    end
-  end
-  mon.write(rpad2(tostring(round(value,3))).." "..unit)
+function printnocolor(name, value, unit)
+  mon.write(rpad1(name) .. ": " .. rpad2(tostring(round(value, 3))) .. " " .. unit)
   newline()
 end
 
-function printEnergy(energy,max)
-  mon.write(rpad1('Energy')..": ")
-  local percent = math.floor(energy/math.abs(max)*100)
-  if percent > 66 then mon.setTextColor(colors.green)
-  elseif percent < 34 then mon.setTextColor(colors.red)
-  else mon.setTextColor(colors.yellow)
+function printcolor(name, value, maxvalue, unit)
+  mon.write(rpad1(name) .. ": ")
+  local percent = math.floor(value / math.abs(maxvalue) * 100)
+  if maxvalue > 0 then
+    if percent > 66 then
+      mon.setTextColor(colors.green)
+    elseif percent < 34 then
+      mon.setTextColor(colors.red)
+    else
+      mon.setTextColor(colors.yellow)
+    end
+  else
+    if percent > 66 then
+      mon.setTextColor(colors.red)
+    elseif percent < 34 then
+      mon.setTextColor(colors.green)
+    else
+      mon.setTextColor(colors.yellow)
+    end
+  end
+  mon.write(rpad2(tostring(round(value, 3))) .. " " .. unit)
+  newline()
+end
+
+local function updateInfo()
+  energy = toFE(cube.getEnergy())
+  change = toFE(cube.getLastInput() - cube.getLastOutput())
+  percent = cube.getEnergyFilledPercentage()
+end
+
+local function printInfo()
+  reset()
+  local color = colors.yellow
+  if percent > 66 then
+    color = colors.green
+  elseif percent < 34 then
+    color = colors.red
   end
   local e = formatEnergy(energy)
-  local m = formatEnergy(max)
-  mon.write(rpad2(e).."/"..m)
+  local m = formatEnergy(maxEnergy)
+  mon.write(rpad1('Energy') .. ": ")
+  mon.setTextColor(color)
+  mon.write(rpad2(e) .. "/" .. m)
+  newline()
+
+  color = ternary(change > 0, colors.green, colors.red)
+  mon.write(rpad1('Yield') .. ": ")
+  mon.setTextColor(color)
+  mon.write(rpad2(round(change, 3)) .. " FE/t")
   newline()
 end
-function printYield(energy)
-  mon.write(rpad1('Yield')..": ")
-  if energy > 0 then mon.setTextColor(colors.green)
-  else mon.setTextColor(colors.red) end
-  --local e = formatEnergy(energy)
-  mon.write(rpad2(round(energy,3)).." FE/t")
-  newline()
-end
-function setRedstone(percent)
-  if percent < 50 then 
+
+sleep(1)
+print(maxEnergy)
+while true do
+  updateInfo()
+  for _, cmon in pairs(monitors) do
+    mon = cmon
+    printInfo()
+  end
+  --print(percent)
+  if percent < 50 then
     redstone.setOutput(output, true)
   elseif percent > 95 then
     redstone.setOutput(output, false)
   end
-end
-
-print(maxEnergy)
-while true do
-  local energy = cube.getEnergy()
-  local percent = energy/maxEnergy*100
-  local change = (energy-oldEnergy)/(R*20)
-  oldEnergy = energy
-  for _,cmon in pairs(monitors) do
-    mon = cmon  
-    reset()
-    printEnergy(energy,maxEnergy)
-    printYield(change)
-  end
-  --print(percent)
-  setRedstone(percent)
   sleep(R)
 end
